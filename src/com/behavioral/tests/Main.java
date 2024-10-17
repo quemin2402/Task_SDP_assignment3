5481a802cc27b57a1c0d7818608bfdd0c6197446
quemin2402 <mereyibraim2006@gmail.com>
2024-10-17 15:24:45 +0500

    main class changes

--- src/com/behavioral/tests/Main.java
+++ src/com/behavioral/tests/Main.java
@@ -15,37 +15,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем задачи (арифметические операции)
         Task task1 = new Task("Subtract operation", 10, 5, "subtract");
         Task task2 = new Task("Add operation", 4, 6, "add");
         Task task3 = new Task("Multiply operation", 7, 8, "multiply");
 
-        // Создаем список задач
         List<Task> taskList = new ArrayList<>();
         taskList.add(task1);
         taskList.add(task2);
         taskList.add(task3);
 
-        // Сортируем задачи по приоритету (по убыванию)
         taskList.sort(Comparator.comparing(Task::getPriority).reversed());
 
-        // Создаем итератор задач
         TaskIterator taskIterator = new TaskListIterator(taskList);
 
-        // Создаем медиатор
         Mediator mediator = new TaskMediator();
 
-        // Обрабатываем задачи через итератор, создаем и выполняем команды
         while (taskIterator.hasNext()) {
             Task task = taskIterator.next();
-
-            // Создаем команду для задачи
             Command taskCommand = new TaskCommand(task);
-
-            // Выполняем команду
             taskCommand.execute();
-
-            // Передаем задачу в медиатор для дальнейшей обработки
             mediator.processTask(task);
         }
     }
